7b1ab95ecd95b7b10cd140fd6b096f751f55cd8e
MahmoudZeert <139872270+MahmoudZeert@users.noreply.github.com>
2023-07-18 17:22:53 +0300

    Delete PropertyDamageCompensation.Domain/EntitiesCopy directory

--- PropertyDamageCompensation.Domain/EntitiesCopy/Floor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PropertyDamageCompensation.Domain.EntitiesCopy
-{
-    public class Floor
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public ICollection<Application>? Application { get; set; }
-    }
-}
--- PropertyDamageCompensation.Domain/EntitiesCopy/PropertyType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PropertyDamageCompensation.Domain.EntitiesCopy
-{
-    public class PropertyType
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public ICollection<Application>? Application { get; set; }
-    }
-}
