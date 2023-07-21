66b1cbc91754f4fa15f733ef3d777a787053c08d
Mahmoud Elzeert <139872270+MahmoudZeert@users.noreply.github.com>
2023-07-21 14:22:23 +0300

    Update 20230202074333_IntialSetup.cs

--- PropertyDamageCompensation.Web/Data/Migrations/20230202074333_IntialSetup.cs
+++ PropertyDamageCompensation.Web/Data/Migrations/20230202074333_IntialSetup.cs
@@ -23,7 +23,7 @@ namespace PropertyDamageCompensation.Web.Data.Migrations
 
             // Create the admin user if it doesn't exist
             migrationBuilder.Sql("INSERT INTO AspNetUsers (Id, UserName, NormalizedUserName, Email, NormalizedEmail, EmailConfirmed, LockoutEnabled, SecurityStamp, PasswordHash,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,AccessFailedCount,IsApplicant) " +
-                                 "VALUES ('1', 'mahmoudyzeort@gmail.com', 'MAHMOUDYZEORT@GMAIL.COM', 'mahmoudyzeort@gmail.com', 'MAHMOUDYZEORT@GMAIL.COM', 1, 0, '', 'AQAAAAEAACcQAAAAEK+M5NtwcMFgKAhQ+TPU+EGL8c3NemOISUIAOFaiOOWqedLUaIOxDDO8Wph+blRm6A==','03575359',0,0,0,0)");
+                                 "VALUES ('1', 'adminpdc@gmail.com', 'ADMINPDC@GMAIL.COM', ''adminpdc@gmail.com', 'ADMINPDC@GMAIL.COM', 1, 0, '', 'AQAAAAEAACcQAAAAEK+M5NtwcMFgKAhQ+TPU+EGL8c3NemOISUIAOFaiOOWqedLUaIOxDDO8Wph+blRm6A==','',0,0,0,0)");
 
             // Assign the admin user to the "Admin" role if not already assigned
             migrationBuilder.Sql("INSERT INTO AspNetUserRoles (UserId, RoleId) VALUES ('1', '1')");
