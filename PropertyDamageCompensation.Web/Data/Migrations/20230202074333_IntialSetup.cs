bc56f42b546510a04958574ef8cfa25f47c06b8f
Mahmoud Elzeert <139872270+MahmoudZeert@users.noreply.github.com>
2023-07-22 11:28:24 +0300

    Update 20230202074333_IntialSetup.cs

--- PropertyDamageCompensation.Web/Data/Migrations/20230202074333_IntialSetup.cs
+++ PropertyDamageCompensation.Web/Data/Migrations/20230202074333_IntialSetup.cs
@@ -23,7 +23,7 @@ namespace PropertyDamageCompensation.Web.Data.Migrations
 
             // Create the admin user if it doesn't exist
             // to login to the system the password is Myz@23
-            // You have to create this gmail account for the admin in order to be able to receive emails
+            // You have to create this gmail account at gmail platform for the admin in order to be able to receive emails
             // But, you must use this password :  Myz@23, to login to the system because the passwordhash used is for this password !!!!
             migrationBuilder.Sql("INSERT INTO AspNetUsers (Id, UserName, NormalizedUserName, Email, NormalizedEmail, EmailConfirmed, LockoutEnabled, SecurityStamp, PasswordHash,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,AccessFailedCount,IsApplicant) " +
                                  "VALUES ('1', 'adminpdc@gmail.com', 'ADMINPDC@GMAIL.COM', ''adminpdc@gmail.com', 'ADMINPDC@GMAIL.COM', 1, 0, '', 'AQAAAAEAACcQAAAAEK+M5NtwcMFgKAhQ+TPU+EGL8c3NemOISUIAOFaiOOWqedLUaIOxDDO8Wph+blRm6A==','',0,0,0,0)");
