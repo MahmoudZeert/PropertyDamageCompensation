5ad46d0674c3f9b17f21ff401943feef04210561
Mahmoud Elzeert <139872270+MahmoudZeert@users.noreply.github.com>
2023-07-24 17:52:43 +0300

    Update 20230202074333_IntialSetup.cs

--- PropertyDamageCompensation.Web/Data/Migrations/20230202074333_IntialSetup.cs
+++ PropertyDamageCompensation.Web/Data/Migrations/20230202074333_IntialSetup.cs
@@ -26,7 +26,7 @@ namespace PropertyDamageCompensation.Web.Data.Migrations
             // You have to create this gmail account at gmail platform for the admin in order to be able to receive emails
             // But, you must use this password :  Myz@23, to login to the system because the passwordhash used is for this password !!!!
             migrationBuilder.Sql("INSERT INTO AspNetUsers (Id, UserName, NormalizedUserName, Email, NormalizedEmail, EmailConfirmed, LockoutEnabled, SecurityStamp, PasswordHash,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,AccessFailedCount,IsApplicant) " +
-                                 "VALUES ('1', 'adminpdc@gmail.com', 'ADMINPDC@GMAIL.COM', ''adminpdc@gmail.com', 'ADMINPDC@GMAIL.COM', 1, 0, '', 'AQAAAAEAACcQAAAAEK+M5NtwcMFgKAhQ+TPU+EGL8c3NemOISUIAOFaiOOWqedLUaIOxDDO8Wph+blRm6A==','',0,0,0,0)");
+                                 "VALUES ('1', 'adminpdc@gmail.com', 'ADMINPDC@GMAIL.COM', 'adminpdc@gmail.com', 'ADMINPDC@GMAIL.COM', 1, 0, '', 'AQAAAAEAACcQAAAAEK+M5NtwcMFgKAhQ+TPU+EGL8c3NemOISUIAOFaiOOWqedLUaIOxDDO8Wph+blRm6A==','',0,0,0,0)");
 
             // Assign the admin user to the "Admin" role if not already assigned
             migrationBuilder.Sql("INSERT INTO AspNetUserRoles (UserId, RoleId) VALUES ('1', '1')");
